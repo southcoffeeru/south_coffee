ab78c25e4db662ca31902022d89cb459efd1e3d2
Yury Bogomolov <cleveryura@gmail.com>
2022-04-08 14:29:25 +0300

    close sessions after handler execution

--- tg-bot/database.py
+++ tg-bot/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 import settings
+import logger
 
 
 def init():
@@ -10,5 +11,8 @@ def init():
                        pool_size=settings.CONFIG['db']['query_pool_size'])
 
 
-def session():
-    return Session(bind=db)
+def session() -> Session:
+    session = Session(bind=db)
+    logger.logger.debug(
+        'Acquire new session, Engine status: {}'.format(db.pool.status()))
+    return session
